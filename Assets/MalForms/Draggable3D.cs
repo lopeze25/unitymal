bc17ee6066b9dd21689538d541f002bb3a3aa0e2
James Vanderhyde <vanderhyde@sxu.edu>
2021-12-03 17:35:56 -0600

    Moved entity close-up code from Draggable3D to MalEntity

--- Assets/MalForms/Draggable3D.cs
+++ Assets/MalForms/Draggable3D.cs
@@ -9,24 +9,10 @@ using Mal;
 public class Draggable3D : MonoBehaviour
 {
     private MalPrinter formPrinter;
-    private Camera objectCam;
-    private int objectCamLayer;
-    private Texture2D tex;
-    private int oldLayer;
-    private Renderer rend;
 
     void Start()
     {
         this.formPrinter = GameObject.Find("Canvas").transform.GetComponentInChildren<MalPrinter>();
-        this.objectCam = GameObject.Find("Close-up camera").GetComponent<Camera>();
-        this.objectCamLayer = 0;
-        int mask = this.objectCam.cullingMask;
-        while (mask > 1)
-        {
-            mask >>= 1;
-            this.objectCamLayer++;
-        }
-        this.rend = this.GetComponent<Renderer>();
     }
 
     void OnMouseDown()
@@ -34,40 +20,6 @@ public class Draggable3D : MonoBehaviour
         //Create the Mal form
         MalEntity result;
         result = (MalEntity)formPrinter.pr_form(new types.MalObjectReference(this.gameObject));
-
-        //Create the sprite for the form
-        RenderTexture rtex = this.objectCam.targetTexture;
-        this.tex = new Texture2D(rtex.width, rtex.height, TextureFormat.RGBA32, false);
-        result.SetSprite(Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0f, 1f), 100.0f));
-
-        //Focus the camera on the 3D object
-        this.oldLayer = this.gameObject.layer;
-        this.gameObject.layer = this.objectCamLayer;
-        float radius = this.rend.bounds.extents.magnitude;
-        this.objectCam.transform.position = Camera.main.transform.position;
-        this.objectCam.transform.LookAt(this.transform);
-        this.objectCam.orthographicSize = radius;
-
-        //Tell the camera to render to the sprite next frame
-        StartCoroutine(CopyTextureToSprite());
     }
 
-    IEnumerator CopyTextureToSprite()
-    {
-        //Wait for next frame
-        yield return null;
-
-        //Software texture copy
-        //RenderTexture old_rt = RenderTexture.active;
-        //RenderTexture.active = this.objectCam.targetTexture;
-        //this.tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
-        //this.tex.Apply();
-        //RenderTexture.active = old_rt;
-
-        //Hardware texture copy
-        Graphics.CopyTexture(this.objectCam.targetTexture, this.tex);
-
-        //Switch the object back to its normal
-        this.gameObject.layer = oldLayer;
-    }
 }
--- Assets/MalForms/MalEntity.cs
+++ Assets/MalForms/MalEntity.cs
@@ -13,16 +13,20 @@ public class MalEntity : MalForm
     [SerializeField]
     public GameObject value = null;
 
+    private Camera objectCam;
+    private Texture2D tex;
+    private int oldLayer;
+
     void Awake()
     {
         if (value == null)
             value = GameObject.Find("World");
     }
 
-    public void SetSprite(Sprite s)
+    void Start()
     {
-        Image im = this.transform.GetChild(0).GetComponent<Image>();
-        im.sprite = s;
+        if (value != GameObject.Find("World"))
+            this.TakePicture();
     }
 
     public override types.MalVal read_form()
@@ -30,4 +34,61 @@ public class MalEntity : MalForm
         return new types.MalObjectReference(value);
     }
 
+    private void TakePicture()
+    {
+        //Set up camera for close-up
+        this.objectCam = GameObject.Find("Close-up camera").GetComponent<Camera>();
+        int objectCamLayer = 0;
+        int mask = this.objectCam.cullingMask;
+        while (mask > 1)
+        {
+            mask >>= 1;
+            objectCamLayer++;
+        }
+
+        //Create the sprite for the form
+        RenderTexture rtex = this.objectCam.targetTexture;
+        this.tex = new Texture2D(rtex.width, rtex.height, TextureFormat.RGBA32, false);
+        Image im = this.transform.GetChild(0).GetComponent<Image>();
+        im.sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0f, 1f), 100.0f);
+
+        //Focus the camera on the 3D object
+        this.oldLayer = this.value.layer;
+        this.value.layer = objectCamLayer;
+        Renderer rend = this.value.GetComponent<Renderer>();
+        //If there is no renderer, it may be a group object. In that case, we'll have to combine all the children into one picture.
+        //We should do that in any case, in case some child objects stick out from the parent object.
+        float radius = rend.bounds.extents.magnitude;
+        this.objectCam.transform.position = Camera.main.transform.position;
+        this.objectCam.transform.LookAt(this.value.transform);
+        this.objectCam.orthographicSize = radius;
+
+        //Tell the camera to render to the sprite next frame
+        StartCoroutine(CopyTextureToSprite());
+    }
+
+    private IEnumerator CopyTextureToSprite()
+    {
+        //Wait for next frame
+        yield return null;
+
+        bool useHardwareTextureCopy = true;
+        if (useHardwareTextureCopy)
+        {
+            //Hardware texture copy
+            Graphics.CopyTexture(this.objectCam.targetTexture, this.tex);
+        }
+        else
+        {
+            //Software texture copy
+            RenderTexture old_rt = RenderTexture.active;
+            RenderTexture.active = this.objectCam.targetTexture;
+            this.tex.ReadPixels(new Rect(0, 0, this.tex.width, this.tex.height), 0, 0);
+            this.tex.Apply();
+            RenderTexture.active = old_rt;
+        }
+
+        //Switch the object back to its normal layer
+        this.value.layer = this.oldLayer;
+    }
 }
