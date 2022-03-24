0f43ec6da2ab239d132a063b887b8fc9f5217cdc
James Vanderhyde <vanderhyde@sxu.edu>
2022-03-24 17:50:46 -0500

    Rename all child symbols

--- Assets/Programming UI/SymbolTracker.cs
+++ Assets/Programming UI/SymbolTracker.cs
@@ -20,14 +20,34 @@ public class SymbolTracker : MonoBehaviour
         return this.transform.GetChild(1).GetComponentInChildren<MalSymbol>().GetSymbolName();
     }
 
+    private List<MalSymbol> GetSymbols()
+    {
+        //Get all the symbols
+        Transform df = this.GetComponentInParent<DefiningForm>().transform;
+        MalSymbol[] symbols = df.GetComponentsInChildren<MalSymbol>();
+
+        //Find symbols that match the name
+        List<MalSymbol> nameMatches = new List<MalSymbol>();
+        foreach (MalSymbol s in symbols)
+            if (s.GetSymbolName().Equals(this.GetSymbolName()))
+                nameMatches.Add(s);
+
+        return nameMatches;
+
+        //Filter out the ones whose defining form is not this one's.
+    }
+
     public void CreateSymbolFromField(string symbolName)
     {
+        //Pull the new name from the input field, if not specified by the parameter
         if (symbolName.Equals(""))
-            symbolName = transform.GetChild(0).GetComponentInChildren<TMPro.TMP_InputField>().text;
+            symbolName = this.transform.GetChild(0).GetComponentInChildren<TMPro.TMP_InputField>().text;
 
-        MalSymbol symbol = this.GetComponentInChildren<MalSymbol>();
-        symbol.SetSymbolName(symbolName);
+        //Get all the symbols in child forms that match the names
+        List<MalSymbol> symbols = this.GetSymbols();
 
         //Rename all the symbols in child forms
+        foreach (MalSymbol s in symbols)
+            s.SetSymbolName(symbolName);
     }
 }
