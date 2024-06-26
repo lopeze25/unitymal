d837be8e375a8d3f36e9a739b6e31e6ba218c122
lopeze25 <112291194+lopeze25@users.noreply.github.com>
2024-06-26 10:17:31 -0500

    Expandable buttons
    Added script to buttons to expand them on hover.

---
+++ Assets/ExpandOnHover.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VRButtonExpand : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private RectTransform rectTransform;
+    private Vector2 originalSize;
+    public float expandFactor = 5f; // How much bigger the button gets
+    public float speed = 20f; // Speed of the expansion
+
+    private bool isHovered = false;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        originalSize = rectTransform.sizeDelta;
+    }
+
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+        Vector2 targetSize = isHovered ? originalSize * expandFactor : originalSize;
+        rectTransform.sizeDelta = Vector2.Lerp(rectTransform.sizeDelta, targetSize, deltaTime * speed);
+    }
+
+    // When pointer hovers over the button
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovered = true;
+    }
+
+    // When pointer exits the button
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovered = false;
+    }
+}
