b3338d9bdd86948fa9d6b45cdad54af8b10d41c0
James Vanderhyde <vanderhyde@sxu.edu>
2024-07-03 09:38:44 -0500

    Added shelf for namespace

---
+++ Assets/Programming UI/NameShelf.cs
@@ -0,0 +1,19 @@
+//Part of the programming UI gallery that can add names
+//Created by James Vanderhyde, 3 July 2024
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameShelf : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
