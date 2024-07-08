bb667ef8d04a76e3893e5f9b9d3d4e8f01f5b725
lopeze25 <112291194+lopeze25@users.noreply.github.com>
2024-07-08 01:36:06 -0500

    Menu to Create Objects
    Added a menu to gallery manager to allow users to choose which objects to spawn

--- Assets/GalleryManager.cs
+++ Assets/GalleryManager.cs
@@ -3,16 +3,85 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GalleryManager : MonoBehaviour
 {
     public SaveLoad gallery;
+    public GameObject menuGameObject;
+    public Button cat;
+    public Button cylinder;
+    public Button pig;
+    public Button program;
+    // public void OnCreateObject(InputAction.CallbackContext context)
+    // {
+    //  if (context.phase==InputActionPhase.Performed )
+    //     gallery.Load("(create-entity {:gallery-name \"Cylinder\"})");
+    //  }
 
-    public void OnCreateObject(InputAction.CallbackContext context)
+    public void ToggleMenu(InputAction.CallbackContext context)
     {
-        if (context.phase==InputActionPhase.Performed)
-            gallery.Load("(create-entity {:gallery-name \"Cylinder\"})");
+        Debug.Log("Toggled");
+        if (context.phase == InputActionPhase.Performed)
+        {
+            if (menuGameObject.activeSelf == true)
+                menuGameObject.SetActive(false);
+            else
+            {
+                menuGameObject.SetActive(true);
+                OnVRSelect();
+
+            }
+        }
     }
+
+
+    public void OnVRSelect()
+    {
+
+        //Put the canvas in front of the user
+        Canvas c = this.menuGameObject.transform.GetComponentInParent<Canvas>();
+        Transform head = Camera.main.transform;
+
+        c.transform.position = head.position;
+        c.transform.forward = head.forward;
+        c.transform.Translate(new Vector3(0, 0, 0.2f));
+        Transform imageTransform = c.transform.Find("Menu");
+            imageTransform.localScale = new Vector3(0.5f, 0.20f, 0.11f);
+}
+
+
+    private void Start()
+    {
+        cat.onClick.AddListener(CreateCat);
+        cylinder.onClick.AddListener(CreateCylinder);
+        pig.onClick.AddListener(CreatePig);
+        program.onClick.AddListener(CreateProgram);
+        Debug.Log("ListenersAdded");
+    }
+
+    public void CreateCylinder()
+    {
+        gallery.Load("(create-entity {:gallery-name \"Cylinder\"})");
+    }
+    public void CreateCat()
+    {
+        gallery.Load("(create-entity {:gallery-name \"Cat\"})");
+    }
+    public void CreatePig()
+    {
+        gallery.Load("(create-entity {:gallery-name \"Pig\"})");
+    }
+
+    public void CreateProgram()
+    {
+        Debug.Log("Program Called");
+        gallery.Load("(create-entity {:gallery-name \"Program\"})");
+    }
+
 }
